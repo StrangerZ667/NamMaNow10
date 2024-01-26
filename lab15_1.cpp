d3e937a5a35bb8736c9caae5d710aa6659485851
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-01-26 14:17:00 +0700

    Lab15_1 Updated

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,20 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+    int a = 5;
+    char b = 'A';
+    char *c = &b, *y = &b;
+    int *x = &a, **z = &x;
+    cout << a << " " << b << " " << *c << " " << x << " " << &y << " " << z << endl;
+    cout << &a << " " << &y << " " << &c << " " << &x << " " << &y << " " << &z << endl;
+    b = 'F';
+    cout << a << " " << b << " " << *c << " " << x << " " << &y << " " << z << endl;
+    *y = 'W';
+    cout << a << " " << b << " " << *c << " " << x << " " << &y << " " << z << endl;
+    *x = 6;
+    cout << a << " " << b << " " << *c << " " << x << " " << &y << " " << z << endl;
+    **z = 7;
+    cout << a << " " << b << " " << *c << " " << x << " " << &y << " " << z << endl;
 	return 0;
 }
